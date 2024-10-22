19220e585de81989660fbd544ccc9515fff0d6ac
Royangliu <royangliu@gmail.com>
2024-10-22 19:51:18 -0400

    task 2.1 finish

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current logged-in user.
+     * @param name the name of current the user
+     */
+    void setCurrentUser(String name);
 }
